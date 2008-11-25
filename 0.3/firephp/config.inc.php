737cda50bcd1cae837dff95c6a1e81107479b2b3
jeffe <jeffe@9ca40523-fe58-4fde-b06a-e7437bdac29b>
2008-11-25 04:58:27 +0000

    header korrigiert
    git-svn-id: svn://svn.rexdev.de/addons/firephp/trunk@11 9ca40523-fe58-4fde-b06a-e7437bdac29b

--- 0.3/firephp/config.inc.php
+++ 0.3/firephp/config.inc.php
@@ -2,10 +2,7 @@
 /**
 * FirePHP Addon
 *
-* @author rexdev[at]f-stop[dot]de Jan Camrda
-* @author <a href="http://rexdev.f-stop.de">rexdev.f-stop.de</a>
-*
-* FirePHP Lib Copyright (c) 2006-2008, Christoph Dorn
+* FirePHP Lib Copyright (c) 2006-2008, Christoph Dorn, http://firephp.org
 * FirePHP Lib v 0.2.1
 *
 * @author rexdev[at]f-stop[dot]de Jan Camrda
--- 0.3/firephp/help.inc.php
+++ 0.3/firephp/help.inc.php
@@ -2,22 +2,16 @@
 /**
 * FirePHP Addon
 *
-* @author rexdev[at]f-stop[dot]de Jan Camrda
-* @author <a href="http://rexdev.f-stop.de">rexdev.f-stop.de</a>
-*
-* FirePHP Lib Copyright (c) 2006-2008, Christoph Dorn
+* FirePHP Lib Copyright (c) 2006-2008, Christoph Dorn, http://firephp.org
 * FirePHP Lib v 0.2.1
 *
 * @author rexdev[at]f-stop[dot]de Jan Camrda
 * @author <a href="http://rexdev.f-stop.de">rexdev.f-stop.de</a>
 *
 * @package redaxo4
-* @version 0.3
+* @version 0.3 
 * $Id$: 
 */
-// error_reporting(E_ALL ^ E_NOTICE); // Notices ausschalten
-@ ini_set('error_reporting', E_ALL);
-@ ini_set('display_errors', On);
 
 if ( !isset( $mode)) $mode = '';
 switch ($mode)
