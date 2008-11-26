5c96d7f450948fa6d763b94de74fb7f29eac83b4
jeffe <jeffe@9ca40523-fe58-4fde-b06a-e7437bdac29b>
2008-11-26 12:13:30 +0000

    umgeschrieben, linkgenerierung für subpages, install wird abgefangen für sicherheitshinweis
    git-svn-id: svn://svn.rexdev.de/addons/firephp/trunk@14 9ca40523-fe58-4fde-b06a-e7437bdac29b

--- 0.3/firephp/help.inc.php
+++ 0.3/firephp/help.inc.php
@@ -13,7 +13,54 @@
 * $Id$: 
 */
 
-if ( !isset( $mode)) $mode = '';
+echo '
+<style type="text/css" media="screen">
+<!--
+.backendoverride {font-size:100%;margin:0;padding:0}
+.backendoverride h1 {font-size:20px;margin:0 0 8px 0;padding:0}
+.backendoverride h2 {font-size:18px;margin:7px 0;padding:0}
+.backendoverride h3 {font-size:16px;margin:6px 0;padding:0}
+.backendoverride h4 {font-size:12px;margin:5px 0;padding:0}
+.backendoverride h5 {font-size:10px;margin:4px 0;padding:0}
+.backendoverride ul {margin:0;padding:2px 0 6px 20px;}
+.backendoverride li {list-style-type:square;margin:0;padding:0;}
+.backendoverride cite {color:blue;font-size:10px;font-family:monospace;font-style:normal;}
+.backendoverride pre {color:blue;border:1px dashed #C0C0C0;background:#FAF9F5;font-size:12px;margin:6px 4px 6px 0;padding:8px;font-family:monospace;}
+.backendoverride hr {height:1px;border:0;border-top:1px solid silver;margin:8px 0;padding:0;}
+.backendoverride p.helpnav {background:transparent;margin:0;padding:0;font-weight:bold;}
+-->
+</style>
+<div class="backendoverride">
+';
+
+// Addon Identifier
+$mypage = "firephp";
+
+// Subnavigation Items
+$helppages = array (''             => 'Addon Hilfe', 
+										'changelog'    => 'Addon Changelog', 
+										'libchangelog' => 'FirePHP Changelog',
+										'libreadme'    => 'FirePHP Readme',
+										'liblicense'   => 'FirePHP License');
+
+if (!isset($mode)) $mode = '';
+
+// Build Help Subnavigation
+$helpnav = '';
+foreach ($helppages as $helppage => $name)
+{
+	if ($mode != $helppage)
+	{
+	$helpnav .= ' | <a href="?page=addon&spage=help&addonname='.$mypage.'&mode='.$helppage.'">'.$name.'</a>';
+	}
+	else
+	{
+	$helpnav .= ' | '.$name;
+	}
+}
+echo '<p class="helpnav">'.ltrim($helpnav, " | ").'</p><hr />';
+
+// Assign Include Files
 switch ($mode)
 {
 	case 'changelog':
@@ -28,28 +75,12 @@ switch ($mode)
 	case 'libreadme':
 		$file = 'FirePHPCore/README';
 		break;
+	case 'installcatch':
+		$file = '_warning.txt';
+		break;
 		
   default: $file = '_readme.txt'; 
 }
-
-echo '
-<style type="text/css" media="screen">
-<!--
-.backendoverride {font-size:100%;margin:0;padding:0}
-.backendoverride h1 {font-size:20px;margin:0 0 8px 0;padding:0}
-.backendoverride h2 {font-size:18px;margin:7px 0;padding:0}
-.backendoverride h3 {font-size:16px;margin:6px 0;padding:0}
-.backendoverride h4 {font-size:12px;margin:5px 0;padding:0}
-.backendoverride h5 {font-size:10px;margin:4px 0;padding:0}
-.backendoverride ul {margin:0;padding:2px 0 6px 20px;}
-.backendoverride li {list-style-type:square;margin:0;padding:0;}
-.backendoverride cite {color:blue;font-size:10px;font-family:monospace;font-style:normal;}
-.backendoverride pre {color:blue;font-size:12px;margin:10px;font-family:monospace}
-.backendoverride hr {height:1px;border:0;border-top:1px solid silver;margin:8px 0;padding:0;}
--->
-</style>
-<div class="backendoverride">
-';
 
 $file = dirname( __FILE__) .'/'. $file;
 $fh = fopen($file, 'r');
