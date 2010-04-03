f46a2e1f62aebc6a72318d8e850758b8234ffd9e
jeffe <jeffe@9ca40523-fe58-4fde-b06a-e7437bdac29b>
2010-04-03 20:03:55 +0000

    css: über include funktion
    git-svn-id: svn://svn.rexdev.de/addons/firephp/trunk@59 9ca40523-fe58-4fde-b06a-e7437bdac29b

---
+++ firephp/functions/function.rex_firephp_css_add.inc.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* FirePHP Addon
+*
+* FirePHP Lib Copyright (c) 2006-2010, Christoph Dorn, http://firephp.org
+* FirePHP Lib v 0.3.1 & 0.3.2rc1
+*
+* @author <a href="http://rexdev.de">rexdev.de</a>
+*
+* @package redaxo4
+* @version 0.4.1
+* $Id$: 
+*/
+
+function rex_firephp_css_add($params) {
+  $n ="\n";
+  $params['subject'] .= $n.'<link rel="stylesheet" type="text/css" href="../files/addons/firephp/rex_firephp_backend.css" />'.$n;
+  return $params['subject'];
+}
+?>
--- firephp/pages/help.inc.php
+++ firephp/pages/help.inc.php
@@ -19,27 +19,6 @@ $subpage = rex_request('subpage', 'string');
 $chapter = rex_request('chapter', 'string');
 $func = rex_request('func', 'string');
 
-echo '
-<style type="text/css" media="screen">
-<!--
-.backendoverride {font-size:100%;margin:0;padding:0}
-.backendoverride h1 {font-size:20px;margin:0 0 8px 0;padding:0}
-.backendoverride h2 {font-size:18px;margin:7px 0;padding:0}
-.backendoverride h3 {font-size:16px;margin:6px 0;padding:0}
-.backendoverride h4 {font-size:12px;margin:5px 0;padding:0}
-.backendoverride h5 {font-size:10px;margin:4px 0;padding:0}
-.backendoverride ul {margin:0;padding:2px 0 6px 20px;}
-.backendoverride li {list-style-type:square;margin:0;padding:0;}
-.backendoverride cite {color:blue;font-size:10px;font-family:monospace;font-style:normal;}
-.backendoverride pre {color:blue;border:1px dashed #C0C0C0;background:#FAF9F5;font-size:12px;margin:6px 4px 6px 0;padding:8px;font-family:monospace;}
-.backendoverride pre code {color:blue;border:0;background:transparent;font-size:12px;margin:0;padding:0;font-family:monospace;}
-.backendoverride pre.plain {color:black;border:0;background:transparent;margin:0;padding:0;font-family:monospace;}
-.backendoverride hr {height:1px;border:0;border-top:1px solid silver;margin:8px 0;padding:0;}
-.backendoverride p.helpnav {background:transparent;margin:0;padding:0;font-weight:bold;}
--->
-</style>
-';
-
 // Addon Identifier
 $mypage = "firephp";
 
@@ -103,7 +82,7 @@ switch ($chapter)
 echo '<div class="rex-addon-output">
   <h2 class="rex-hl2" style="font-size:1em">'.ltrim($chapternav, " | ").'</h2>
   <div class="rex-addon-content">
-	<div class= "backendoverride">';
+	<div class= "firephp">';
 
 $file = $REX['INCLUDE_PATH']. '/addons/firephp/'.$file;
 $fh = fopen($file, 'r');
