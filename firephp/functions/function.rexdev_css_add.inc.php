c689b366ab12881707bb903a31a30ba630de414d
jeffe <jeffe@9ca40523-fe58-4fde-b06a-e7437bdac29b>
2010-04-15 02:28:33 +0000

    comments
    git-svn-id: svn://svn.rexdev.de/addons/firephp/trunk@84 9ca40523-fe58-4fde-b06a-e7437bdac29b

--- firephp/functions/function.rexdev_css_add.inc.php
+++ firephp/functions/function.rexdev_css_add.inc.php
@@ -1,12 +1,25 @@
 <?php
 /**
-* Statische Funktion zum Einbinden von CSS Stylesheets ins Redaxo Backend
+* Generische Funktion zum Einbinden von CSS Stylesheets ins Redaxo Backend
 *
+* Beispiel:
+* ---------------------------------------------------------------
+* $css = '<link rel="stylesheet" type="text/css" href="../files/addons/'.$myself.'/backend.css" />';
+* 
+* if ($REX['REDAXO']) {
+*   include_once $myroot.'/functions/function.rexdev_css_add.inc.php';
+*   rex_register_extension('PAGE_HEADER', 'rexdev_css_add',array('css'=>$css));
+* }
+* ------------------------------------------------------------------------------
+* 
 * @author rexdev.de
 *
 * @package redaxo4
 * @version 0.4.2
 * $Id$: 
+*
+* 
+* 
 */
 
   if(!function_exists('rexdev_css_add'))
